342e4e0e15c1a331503b079f58b2da2be9a0bf9f
zhaohongguang <zhg930824@sina.com>
2020-12-21 17:34:57 +0800

    hometest show

--- pages/testerhome/detail/show.js
+++ pages/testerhome/detail/show.js
@@ -1,18 +1,73 @@
 // pages/testerhome/detail/show.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    topic: { },
+    replies: [],
+    offset: 0,
+    limit: 20
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.requestTopicDetail(options.id)
+    this.requestTopicReplies(options.id)
+  },
 
+  /**
+   * 请求数据topic详情信息
+   */
+  requestTopicDetail: function (id) {
+    var that = this;
+    console.log(that.data.dataArray)
+    wx.request({
+      url: app.globalData.testerhome_url + 'topics/' + id,
+      data: { },
+      method: 'GET',
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          topic: res.data.topic,
+          loadingHidden: true,
+          //maxtime: res.data.info.maxtime
+        })
+      }
+    })
+  },
+
+  requestTopicReplies: function(topic_id) {
+    var that = this;
+    console.log(that.data.dataArray)
+    wx.request({
+      url: app.globalData.testerhome_url + 'topics/' + topic_id + "/replies",
+      data: {
+              offset: that.data.offset,
+              limit: that.data.limit
+       },
+      method: 'GET',
+      success: function (res) {
+        if (res.statusCode==200) {
+          that.setData({
+            replies: that.data.replies.concat(res.data.replies),
+            loadingHidden: true,
+            //maxtime: res.data.info.maxtime
+          })
+        }
+      }
+    })
+  },
+
+  //下拉刷新数据
+  onscrolltolower() {
+    this.data.offset += 20;
+    this.requestTopicReplies('')
   },
 
   /**
